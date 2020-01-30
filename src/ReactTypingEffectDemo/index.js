ad5aa08e2a3d8bb16d18ccb741eadc2a1152fb22
Farai <farai@bwsonline.co.za>
2020-01-30 07:01:44 +0200

    Remove node-sass and css from the demo

--- src/ReactTypingEffectDemo/index.js
+++ src/ReactTypingEffectDemo/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ReactTypingEffect from '../lib';
 
-import './styles/index.scss';
-
 const ReactTypingEffectDemo = () => {
   return (
     <div className="container">
